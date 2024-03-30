060a525940a617a8cd04281eb62564bc780305f7
Victor KVV <kvv_post@mail.ru>
2024-03-30 23:11:37 +0300

    Step 2.1.2 add AppTest

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class AppTest {
+    private Javalin app;
+
+    private static String website;
+    private static String name;
+
+   @BeforeEach
+    public void setUp() throws Exception {
+        app = App.getApp();
+    }
+
+    @AfterEach
+    public void close() {
+        app.stop();
+    }
+
+    @Test
+    public void testMainPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+}
