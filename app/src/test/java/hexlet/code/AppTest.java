c2676bc7669a9bac9fada45b7d25a29eadd87361
KVV <kvvpost@gmail.com>
2024-04-16 17:03:42 +0300

    Сheck 1.3 Url constructor without createdAt

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Timestamp;
+//import java.sql.Timestamp;
 import java.util.Date;
 
 import okhttp3.mockwebserver.MockResponse;
